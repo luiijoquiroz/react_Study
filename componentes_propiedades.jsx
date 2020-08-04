97c013981e5fbd1af7aa2f20d417a58c4a1d49ed
Luis Quiroz <luisjqr_1990@hotmail.com>
2020-08-03 22:40:53 -0400

    All

---
+++ componentes_propiedades.jsx
@@ -0,0 +1,8 @@
+
+// Componentes funcionales y de clase
+
+function Welcome(props) {
+    return <h1>Hello, {props.name}</h1>
+}
+
+
--- renderizando_elementos.jsx
+++ renderizando_elementos.jsx
@@ -1,14 +1,16 @@
+import React from 'react';
 // const element = <h1>Hello, world</h1>;
 // ReactDOM.render(element, document.getElementById("root"));
-
-function tick() {
-    const element = (
-        <div>
-            <h1>Hello World!</h1>
-            <h2>It is {new Date().toLocaleTimeString()}.</h2>
-        </div>
-    )
-    ReactDOM.render( element, document.getElementById('root')
-);
+export default() => {
+    return function tick() {
+        const element = (
+            <div>
+                <h1>Hello World!</h1>
+                <h2>It is {new Date().toLocaleTimeString()}.</h2>
+            </div>
+        )
+        ReactDOM.render( element, document.getElementById('root')
+    );
+    }
+    setInterval(tick, 1000);
 }
-setInterval(tick, 1000);
